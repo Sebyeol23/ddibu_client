d4382942729282384bf74a4b1d43618a5b5bff68
Sebyeol23 <lixxil2323@gmail.com>
2023-12-14 15:36:26 +0900

    flex 적용

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
 import Header from './components/Header';
 import Nav from './components/Nav';
 import Main from './components/Main';
 
 function App() {
   return (
-    <div className="App">
-      <Header className="Header" />
-      <Nav className="Nav" />
-      <Main className="Main" />
+    <div className={styles.App}>
+      <div className={styles.Header}><Header></Header></div>
+      <div className={styles.Nav}><Nav></Nav></div>
+      <div className={styles.Main}><Main></Main></div>
     </div>
   );
 }
