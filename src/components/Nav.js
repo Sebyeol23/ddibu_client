58980cb36b59f175c4c5dd0d43ffae59cf297396
Sebyeol23 <lixxil2323@gmail.com>
2023-12-14 14:09:26 +0900

    Nav.js 추가

---
+++ src/components/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/Nav.css';
+
+function Nav(){
+    return(
+        <div>
+            <h1>This section is navbar</h1>
+        </div>
+    );
+}
+
+export default Nav;
