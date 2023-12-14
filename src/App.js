ab524ea5bc7cd4fe14f3bf0b1fd5e188213bcd43
Sebyeol23 <lixxil2323@gmail.com>
2023-12-14 14:20:19 +0900

    Main.js 추가

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import Nav from './components/Nav';
+import Main from './components/Main';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Nav />
+      <Main />
     </div>
   );
 }
---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/Main.css';
+
+function Main(){
+    return(
+        <div>
+            <h1>This section is main</h1>
+        </div>
+    );
+}
+
+export default Main;
