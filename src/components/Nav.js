4c6df06d117fec8170f87af3656ab02821a380c3
Sebyeol23 <lixxil2323@gmail.com>
2023-12-15 13:33:51 +0900

    nav section 정리

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,7 +4,12 @@ import styles from '../styles/Nav.module.css';
 function Nav(){
     return(
         <div className={styles.navDiv}>
-            <h1 className={styles.navH1}>This section is navbar</h1>
+            <div className={styles.home}>홈</div>
+            <div className={styles.register}>상품 등록</div>
+            <div className={styles.like}>좋아요</div>
+            <div className={styles.chat}>채팅</div>
+            <div className={styles.my}>마이페이지</div>
+            <div className={styles.air}>여백</div>
         </div>
     );
 }
