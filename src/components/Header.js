92d0bac1931fb44864bf135016801a1adb8d91d3
Sebyeol23 <lixxil2323@gmail.com>
2023-12-15 13:09:24 +0900

    header section 정리

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,16 @@ import styles from '../styles/Header.module.css';
 function Header(){
     return(
         <div className={styles.headerDiv}>
-            <div className={styles.searchBox}>This section is searchBox</div>
-            <div className={styles.loginBox}>This section is loginBox</div>
+            <div className={styles.searchBox}>
+                <div className={styles.search}>검색창</div>
+                <div className={styles.sort}>정렬</div>
+                <div className={styles.categorize}>분류</div>
+                <div className={styles.air}>여백</div>
+            </div>
+            <div className={styles.loginBox}>
+                <div className={styles.signUp}>회원가입</div>
+                <div className={styles.signIn}>로그인</div>
+            </div>
         </div>
     );
 }
