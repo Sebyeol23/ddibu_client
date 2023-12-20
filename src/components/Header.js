b363b87727e48cf42d85188904ef1bb2a61c6d72
Sebyeol23 <lixxil2323@gmail.com>
2023-12-20 16:47:56 +0900

    로그인 상태 표시

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import styles from '../styles/Header.module.css';
 import {Link} from 'react-router-dom';
+import axios from 'axios';
 
 function Header(){
+    const [isLoggedIn, setLoggedIn] = useState(false);
+    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiLsnKDsoIAzMyIsImlhdCI6MTcwMzA1ODEyMiwiZXhwIjoxNzAzMDU4NzIyfQ.Xb5aZQM26pQOC9n1k7vV034QIRCTK_K36TRUnvkUB4M";
+
+    async function auth(){
+        await axios.get('http://ec2-15-164-97-56.ap-northeast-2.compute.amazonaws.com/api/home/user', {
+            headers: {
+                Authorization: token
+            }
+        }).then(()=>{
+            setLoggedIn(true);
+        }).catch((error)=>{
+            console.log(error);
+        })
+    }
+
+    useEffect(()=>{
+        auth();
+    }, []);
+
     return(
         <div className={styles.headerDiv}>
             <div className={styles.searchBox}>
@@ -12,6 +32,7 @@ function Header(){
                 <div className={styles.air}>여백</div>
             </div>
             <div className={styles.loginBox}>
+                {isLoggedIn ? <p>로그인 했네</p> : <p>로그인 해라</p>}
                 <Link className={styles.signUp} to={'/signup'}>회원가입</Link>
                 <Link className={styles.signIn} to={'/signin'}>로그인</Link>
             </div>
