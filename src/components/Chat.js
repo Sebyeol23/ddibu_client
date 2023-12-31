3c67d66ed97df996ec9e3309497542eeaff15159
Sebyeol23 <lixxil2323@gmail.com>
2023-12-31 09:39:13 +0900

    채팅방 첫 메세지 표시

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,9 +1,14 @@
 import { useLocation } from 'react-router-dom';
 import styles from '../styles/Chat.module.css';
+import {useState, useEffect} from 'react';
+import axios from 'axios';
 
 function Chat(){
     const location = useLocation();
+    const roomId = location.state && location.state.roomId;
     const partnerId = location.state && location.state.partnerId;
+    const [chats, setChats] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
     const handleSendMessage = () => {
         // if (newMessage.trim() === '') return;
@@ -12,10 +17,31 @@ function Chat(){
         // setNewMessage('');
     }
 
+    useEffect(()=>{
+        async function getMessage(){
+            try{
+                const res = await axios.get('http://ec2-15-164-97-56.ap-northeast-2.compute.amazonaws.com/api/chat/chat', {
+                    headers: {
+                        Authorization: localStorage.getItem("token")
+                    },
+                    params:{
+                        roomId: roomId
+                    }
+                });
+                setChats(res.data);
+                setIsLoading(false);
+            }
+            catch(error){
+                console.log(error);
+            }
+        }
+        getMessage();
+    }, []);
+
     return (
         <div className={styles.app}>
             <div className={styles.header}>{partnerId}</div>
-            <div className={styles.window}></div>
+            <div className={styles.window}>{isLoading ? null : chats[0].message}</div>
             <div className={styles.input}>
                 <input
                     className={styles.inputMessage}
