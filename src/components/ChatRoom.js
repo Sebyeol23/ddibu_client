a88d3b00d23f33071e5a9274ea7cc2a5911a5bae
Sebyeol23 <lixxil2323@gmail.com>
2024-01-04 07:17:10 +0900

    채팅방 리스트 화면 수정

--- src/components/ChatRoom.js
+++ src/components/ChatRoom.js
@@ -1,3 +1,52 @@
+// import styles from '../styles/ChatRoom.module.css';
+// import React, { useState, useEffect } from 'react';
+// import axios from 'axios';
+// import { useNavigate } from 'react-router-dom';
+
+// function ChatRoom({socket}){
+//     const apiUrl = process.env.REACT_APP_API_URL;
+//     const navigate = useNavigate();
+//     const [chatRooms, setChatRooms] = useState([]);
+
+//     useEffect(() => {
+//         async function getChatRoom(){
+//             try {
+//                 const res = await axios.get(`${apiUrl}/api/chat/chat-room`, {
+//                     headers: {
+//                         Authorization: localStorage.getItem("token")
+//                     }
+//                 });
+//                 setChatRooms(res.data);    
+//             } catch (error) {
+//                 console.log(error);
+//             }
+//         };
+//         getChatRoom();
+//         if(!socket) return;
+//         socket.on('newChatRoom', ()=>{
+//             getChatRoom();
+//         });
+
+//         return ()=>{
+//             socket.off('newChatRoom');
+//         }
+//     }, [socket, apiUrl]);
+
+//     return (
+//         <div className={styles.chatRoomBox}>
+//             {chatRooms.length ? 
+//             chatRooms.map((chatRoom, index) => (
+//                 <div className={styles.chatRoom} key={chatRoom.roomId} onClick={()=>{navigate('../chat', {state: {roomId: chatRoom.roomId, partnerId: chatRoom.partnerId}});}}>{`${chatRoom.roomId} -> ${chatRoom.partnerId}`}</div>
+//             ))
+//             :null}
+//         </div>
+//     );
+// }
+
+// export default ChatRoom;
+
+// ChatRoom.js
+
 import styles from '../styles/ChatRoom.module.css';
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
@@ -21,7 +70,9 @@ function ChatRoom({socket}){
                 console.log(error);
             }
         };
+
         getChatRoom();
+
         if(!socket) return;
         socket.on('newChatRoom', ()=>{
             getChatRoom();
@@ -33,12 +84,22 @@ function ChatRoom({socket}){
     }, [socket, apiUrl]);
 
     return (
-        <div className={styles.chatRoomBox}>
-            {chatRooms.length ? 
-            chatRooms.map((chatRoom, index) => (
-                <div className={styles.chatRoom} key={chatRoom.roomId} onClick={()=>{navigate('../chat', {state: {roomId: chatRoom.roomId, partnerId: chatRoom.partnerId}});}}>{`${chatRoom.roomId} -> ${chatRoom.partnerId}`}</div>
-            ))
-            :null}
+        <div className={styles.window}>
+            <div className={styles.chatRoomBox}>
+                {chatRooms.length ? 
+                    chatRooms.map((chatRoom, index) => (
+                        <div
+                            className={styles.chatRoom}
+                            key={chatRoom.roomId}
+                            onClick={() => {
+                                navigate('../chat', { state: { roomId: chatRoom.roomId, partnerId: chatRoom.partnerId } });
+                            }}
+                        >
+                            {`${chatRoom.roomId} -> ${chatRoom.partnerId}`}
+                        </div>
+                    ))
+                : null}
+            </div>
         </div>
     );
 }
