7d2f8cf812529a0783d6df73c8ce35b301513ad3
Sebyeol23 <lixxil2323@gmail.com>
2024-01-04 12:50:42 +0900

    Main 섹션 디자인 수정

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,40 +1,113 @@
+// import React, { useState, useEffect } from 'react';
+// import styles from '../styles/Main.module.css';
+// import InfiniteScroll from 'react-infinite-scroll-component';
+// import axios from 'axios';
+// import {useNavigate} from 'react-router-dom';
+
+// function Main(){
+//   const apiUrl = process.env.REACT_APP_API_URL;
+//   const navigate = useNavigate();
+//   const [items, setItems] = useState([]);
+//   const [hasMore, setHasMore] = useState(true);
+
+//   async function fetchMoreData(){
+//     const res = await axios.get(`${apiUrl}/api/home/product`, {
+//       params:{
+//         limit: 15,
+//         lastId: items[items.length-1].productId
+//       }
+//     });
+//     if(!res.data.length) setHasMore(false);
+//     else setItems([...items, ...res.data]);
+//   };
+
+//   useEffect(() => {
+//     async function fetchData(){
+//       try {
+//         const res = await axios.get(`${apiUrl}/api/home/product`, {
+//           params:{
+//             limit: 15
+//           }
+//         });
+//         if(res.data.length < 15) setHasMore(false);
+//         setItems(res.data);      
+//       } catch (error) {
+//         console.log(error);
+//       }
+//     };
+//     fetchData();
+//   }, [apiUrl]);
+
+//   return (
+//     <div className={styles.mainDiv}>
+//       <InfiniteScroll
+//         className={styles.scroll}
+//         dataLength={items.length}
+//         next={fetchMoreData}
+//         hasMore={hasMore}
+//         loader={<div className={styles.load}>Loading...</div>}
+//         endMessage={<div className={styles.end}>No more items</div>}
+//       >
+//         {items.length > 0 ? items.map((item, index) => (
+//           <div className={styles.item} key={item.productId} onClick={()=>navigate(`/product-info?pid=${item.productId}`)}>
+//             <div className={styles.image} style={{ backgroundImage: `url(data:image/${item.extension};base64,${item.image})` }}>
+//             </div>
+//             <div className={styles.data}>
+//               <div className={styles.name}>{item.title}</div>
+//               <div className={styles.price}>{item.price}</div>
+//             </div>
+//           </div>
+//         )) : null}
+//       </InfiniteScroll>
+//     </div>
+//   );
+// };
+
+// export default Main;
+
+// main.js
+
 import React, { useState, useEffect } from 'react';
 import styles from '../styles/Main.module.css';
 import InfiniteScroll from 'react-infinite-scroll-component';
 import axios from 'axios';
-import {useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
-function Main(){
+function Main() {
   const apiUrl = process.env.REACT_APP_API_URL;
   const navigate = useNavigate();
   const [items, setItems] = useState([]);
   const [hasMore, setHasMore] = useState(true);
 
-  async function fetchMoreData(){
-    const res = await axios.get(`${apiUrl}/api/home/product`, {
-      params:{
-        limit: 15,
-        lastId: items[items.length-1].productId
-      }
-    });
-    if(!res.data.length) setHasMore(false);
-    else setItems([...items, ...res.data]);
-  };
+  async function fetchMoreData() {
+    try {
+      const res = await axios.get(`${apiUrl}/api/home/product`, {
+        params: {
+          limit: 18,
+          lastId: items.length > 0 ? items[items.length - 1].productId : null,
+        },
+      });
+      if (!res.data.length) setHasMore(false);
+      else setItems([...items, ...res.data]);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   useEffect(() => {
-    async function fetchData(){
+    async function fetchData() {
       try {
         const res = await axios.get(`${apiUrl}/api/home/product`, {
-          params:{
-            limit: 15
-          }
+          params: {
+            limit: 18,
+          },
         });
-        if(res.data.length < 15) setHasMore(false);
-        setItems(res.data);      
+        if (res.data.length < 18) setHasMore(false);
+        setItems(res.data);
       } catch (error) {
         console.log(error);
       }
-    };
+    }
     fetchData();
   }, [apiUrl]);
 
@@ -48,19 +121,28 @@ function Main(){
         loader={<div className={styles.load}>Loading...</div>}
         endMessage={<div className={styles.end}>No more items</div>}
       >
-        {items.length > 0 ? items.map((item, index) => (
-          <div className={styles.item} key={item.productId} onClick={()=>navigate(`/product-info?pid=${item.productId}`)}>
-            <div className={styles.image} style={{ backgroundImage: `url(data:image/${item.extension};base64,${item.image})` }}>
+        {items.length > 0 &&
+          items.map((item, index) => (
+            <div
+              className={styles.item}
+              key={item.productId}
+              onClick={() => navigate(`/product-info?pid=${item.productId}`)}
+            >
+              <div
+                className={styles.image}
+                style={{
+                  backgroundImage: `url(data:image/${item.extension};base64,${item.image})`,
+                }}
+              ></div>
+              <div className={styles.data}>
+                <div className={styles.name}>{item.title}</div>
+                <div className={styles.price}>${item.price}</div>
+              </div>
             </div>
-            <div className={styles.data}>
-              <div className={styles.name}>{item.title}</div>
-              <div className={styles.price}>{item.price}</div>
-            </div>
-          </div>
-        )) : null}
+          ))}
       </InfiniteScroll>
     </div>
   );
-};
+}
 
 export default Main;
